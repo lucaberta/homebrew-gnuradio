4f567f89f288d5c0ea4fdd161bd96b6dc06c04b8
Luca Bertagnolio <luca@berta.com>
2014-11-10 13:04:15 +0100

    Update rtlsdr.rb
    Change the Git repo to https://github.com/osmocom/rtl-sdr.git

--- rtlsdr.rb
+++ rtlsdr.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Rtlsdr < Formula
   homepage 'http://sdr.osmocom.org/trac/wiki/rtl-sdr'
-  head 'git://git.osmocom.org/rtl-sdr.git'
+  head 'https://github.com/osmocom/rtl-sdr.git'
 
   depends_on 'pkg-config' => :build
   depends_on 'autoconf' => :build
